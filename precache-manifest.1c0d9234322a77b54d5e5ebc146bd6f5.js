fc630d82eb7f1535361bc206d59e80b7526cc191
Janelle <46268176+Jan729@users.noreply.github.com>
2020-06-01 11:43:14 -0400

    Updates

---
+++ precache-manifest.1c0d9234322a77b54d5e5ebc146bd6f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78c85817692491cb734800c43a60565",
+    "url": "/github.io/star-match/index.html"
+  },
+  {
+    "revision": "67ff96f6edf8694d53d3",
+    "url": "/github.io/star-match/static/css/main.41cb66c5.chunk.css"
+  },
+  {
+    "revision": "dcfa43c5af069ff453c8",
+    "url": "/github.io/star-match/static/js/2.13812af7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/star-match/static/js/2.13812af7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ff96f6edf8694d53d3",
+    "url": "/github.io/star-match/static/js/main.0dc8c144.chunk.js"
+  },
+  {
+    "revision": "3ea6b5db1cdfde5d72b5",
+    "url": "/github.io/star-match/static/js/runtime-main.e0ffca37.js"
+  }
+]);
